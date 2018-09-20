50f0fdaebdad7c816aeac5e466ee68427e387199
Will Salomon <40440339+KingOfPrussia2155@users.noreply.github.com>
2018-09-19 18:41:24 -0600

    git and rando and keys

--- keys.js
+++ keys.js
@@ -1,10 +1,12 @@
-// OMDB API CODE
+console.log("Keys loaded");
 
-http://www.omdbapi.com/?i=tt3896198&apikey=855f5c52
+// Get Spotify API credentials by following these steps:
+// Step One: Visit https://developer.spotify.com/my-applications/#!/
+// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
+// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
+// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down below, you'll need them to use the Spotify API and the node-spotify-api package.
 
-
-
-// Spotify API ID/Secret ID
-
-Client ID 717a00cb9e484186ac63d0018c2ee575
-Client Secret f57ce23f7fd14c5da5a696e084e24cf2 
+exports.spotify = {
+  id: process.env.717a00cb9e484186ac63d0018c2ee575,
+  secret: process.env.57ce23f7fd14c5da5a696e084e24cf
+};
